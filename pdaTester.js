487ae95d941849eaa41cb43cdb10e5255e2e73f9
Matt Harro <111941302+matt-harro@users.noreply.github.com>
2023-11-17 17:28:17 +1000

    initial commit

---
+++ pdaTester.js
@@ -0,0 +1,14 @@
+/** @format */
+
+// ==UserScript==
+// @name         PDA Test Script
+// @namespace    http://torn.city.com.dot.com.com
+// @version      0.1
+// @description  Guess how many busts you can do without getting jailed
+// @author       Ironhydedragon
+// @match        https://www.torn.com/*
+// @license      MIT
+// ==/UserScript==
+
+// URL to fetch the raw userscript from 
+const url = 
