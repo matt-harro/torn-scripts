4f083f1bbe0b2c057a1b87fc257a54b109199aaf
Matt Harro <111941302+matt-harro@users.noreply.github.com>
2023-12-02 02:26:34 +1000

    - added quickBust and quickBail js files and initial code

---
+++ quickBail.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Torn Quick Bail - Everywhere
+// @namespace    http://torn.city.com.dot.com.com
+// @version      0.0.0
+// @description  Quickly bail players from anywhere the bust button appears
+// @author       Ironhydedragon[2428902]
+// @match        https://www.torn.com/*
+// @license      MIT
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+  console.log('💰 Bail Script working'); // TEST
+})();
---
+++ quickBust.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Torn Quick Bust - Everywhere
+// @namespace    http://torn.city.com.dot.com.com
+// @version      0.0.0
+// @description  Quickly bust players from anywhere the bust button appears
+// @author       Ironhydedragon[2428902]
+// @match        https://www.torn.com/*
+// @license      MIT
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+  console.log('💦 Bust Script working'); // TEST
+})();
