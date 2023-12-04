0f2d7a1ab70c6fd9c64b13d8e6afde858f61a774
Matt Harro <111941302+matt-harro@users.noreply.github.com>
2023-12-04 20:19:50 +1000

    - created displayWeaponBonus.js

---
+++ displayWeaponBonus.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         TORN: Display Weapon Bonus + Stats
+// @namespace    http://torn.city.com.dot.com.com
+// @version      1.0.7
+// @description  Displays weapon bonuses + stats next to weapon name in auction house
+// @author       Ironhydedragon
+// @match        https://www.torn.com/amarket.php*
+// @license      MIT
+// @run-at       document-end
+// ==/UserScript==
+
+(() => {
+  console.log('🗡️ Display weapon bonus script is running!'); // TEST
+})();
