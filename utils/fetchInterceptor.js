288ba809f64a5580bcf71bb577b543d55c475f2c
Matt Harro <111941302+matt-harro@users.noreply.github.com>
2023-12-22 21:21:31 +1000

    - updated fetch intercept

--- utils/fetchInterceptor.js
+++ utils/fetchInterceptor.js
@@ -6,17 +6,46 @@
 // @author       Ironhydedragon[2428902]
 // @match        https://www.torn.com/*
 // @license      MIT
-// @grant        none
+// @grant        unsafeWindow
 // @run-at       document-start
 // ==/UserScript==
+
+// ((window) => {
+//   console.log('🎾 Fetch interceptor script is ON!'); // TEST
+
+//   const { fetch: origFetch } = window;
+
+//   window.fetch = async (...args) => {
+//     console.log('fetch called with args:', args);
+
+//     const response = await origFetch(...args);
+
+//     /* work with the cloned response in a separate promise
+//      chain -- could use the same chain with `await`. */
+//     response
+//       .clone()
+//       .json()
+//       .then((data) => console.log('intercepted response data:', data))
+//       .catch((err) => console.error(err));
+
+//     /* the original response can be resolved unmodified: */
+//     return response;
+//   };
+// })(unsafeWindow);
 
 (() => {
   console.log('🎾 Fetch interceptor script is ON!'); // TEST
 
-  const { fetch: origFetch } = window;
+  const { fetch: origFetch } = unsafeWindow;
 
-  window.fetch = async (...args) => {
+  unsafeWindow.fetch = async (...args) => {
     console.log('fetch called with args:', args);
+
+    // if (args[1]?.body?.entries()) {
+    //   for (const entry of args[1].body.entries()) {
+    //     console.log(entry); // TEST
+    //   }
+    // }
 
     const response = await origFetch(...args);
 
@@ -25,7 +54,7 @@
     response
       .clone()
       .json()
-      .then((data) => console.log('intercepted response data:', data))
+      .then((data) => console.log('intercepted response data:', args[0], data))
       .catch((err) => console.error(err));
 
     /* the original response can be resolved unmodified: */
